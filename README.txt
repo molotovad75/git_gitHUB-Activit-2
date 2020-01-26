e282d0820642d7d2e6ad0d8272a8dd834aee29f6
Adrien <adrien.lac@tutanota.com>
2020-01-26 20:34:23 +0100

    Création d'un main (Entre un nombre et renvoie ce nombre)

---
+++ README.txt
@@ -0,0 +1,3 @@
+Bonjour Open classroom,
+
+Ce projet à pour objectif de présenter les notions que j'ai aquises sur GIT et GIT HUB.
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char const *argv[])
+{
+	int nb=0;
+	printf("Entrez un nombre\n");
+	scanf("%d",&nb);
+	printf("Vous avez entrer %d",nb);
+	printf("\n");
+	system("pause");
+	return 0;
+}

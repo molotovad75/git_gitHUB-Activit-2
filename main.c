41d256402115d67338e7f7befee68dd605ec46e9
Adrien <adrien.lac@tutanota.com>
2020-01-26 21:42:46 +0100

    Programme qui change de place l'ordre des nombres

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "inverser_nombre.h"
 
 int main(int argc, char const *argv[])
 {
-	int nb=0;
-	printf("Entrez un nombre\n");
-	scanf("%d",&nb);
-	printf("Vous avez entrer %d",nb);
-	printf("\n");
+	int nb1=0;
+	int nb2=0;
+	printf("Entrez deux nombres\n");
+	scanf("%d %d",&nb1,&nb2);
+	inverser_nombres(&nb1,&nb2);
 	system("pause");
 	return 0;
 }
+
